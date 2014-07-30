4805d94d720a3cfff97cfa5d6a2747f86d86f9ec
Shad Storhaug <shad@shadstorhaug.com>
2014-07-30 19:10:07 +0700

    Created demo showing how to inherit WebViewPage in conjunction with MvcSiteMapProvider

---
+++ MvcSiteMapProvider_InheritView/MvcSiteMapProvider_InheritView/CustomWebViewPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSiteMapProvider_InheritView
+{
+    public class CustomWebViewPage<TModel>
+        : WebViewPage<TModel>
+    {
+        public override void Execute()
+        {
+            // Do nothing.
+        }
+    }
+}
---
+++ MvcSiteMapProvider_InheritView/MvcSiteMapProvider_InheritView/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcSiteMapProvider_InheritView.Startup))]
+namespace MvcSiteMapProvider_InheritView
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
